959de7ee2497da7fdb8b992e82b3378644c2880b
Levy Moreira Cruz <levymcruz@gmail.com>
2025-03-09 11:48:46 -0300

    feat: trocando o botão Fechar por um icone X

--- src/components/fullscreen-modal.tsx
+++ src/components/fullscreen-modal.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { motion, AnimatePresence } from 'framer-motion';
+import { X } from 'lucide-react'; // Importando o ícone de "X"
 import { RootState } from '@/store';
 import { setFullscreen } from '@/store/iframe-editing.slice';
 import SuccessNotification from './success-notification';
@@ -28,32 +29,30 @@ const FullscreenModal: React.FC<FullscreenProps> = ({ selectedFrame, onSave, onC
           exit={{ opacity: 0, scale: 0.95 }}
           transition={{ duration: 0.3, ease: 'easeInOut' }}
         >
-          {/* Barra superior fixa */}
-          <div className="absolute top-0 left-0 w-full bg-gray-900 text-white px-5 py-3 flex justify-end gap-2 shadow-md z-10">
+          <div className="absolute top-0 left-0 w-full bg-gray-900 text-white px-5 py-2 flex justify-end gap-3 shadow-md z-10">
             <button
               onClick={() => onSave(editedHtml)}
-              className="bg-green-600 hover:bg-green-700 text-white px-4 py-2 rounded-lg transition-all"
+              className="bg-green-600 hover:bg-green-700 text-white px-4 py-1 rounded-lg transition-all"
             >
               Salvar
             </button>
             <button
               onClick={() => onCancel()}
-              className="bg-gray-600 hover:bg-gray-700 text-white px-4 py-2 rounded-lg transition-all"
+              className="bg-red-600 hover:bg-red-700 text-white px-2 py-1 rounded-lg transition-all"
             >
               Cancelar
             </button>
             <button
               onClick={() => dispatch(setFullscreen(false))}
-              className="bg-red-600 hover:bg-red-700 text-white px-4 py-2 rounded-lg transition-all"
+              className="text-white p-2 rounded-lg hover:bg-red-700 transition-all"
             >
-              Fechar
+              <X size={18} />
             </button>
           </div>
 
-          {/* Ajuste do iframe para não cobrir a barra superior */}
-          <div className="relative w-full h-full flex justify-center items-center pt-[60px]">
+          <div className="relative w-full h-full flex justify-center items-center pt-[50px]">
             <motion.div
-              className="w-[90%] h-[calc(100%-60px)] bg-white rounded-lg shadow-lg overflow-hidden"
+              className="w-[90%] h-[calc(100%-50px)] bg-white rounded-lg shadow-lg overflow-hidden"
               initial={{ y: 50, opacity: 0 }}
               animate={{ y: 0, opacity: 1 }}
               exit={{ y: 50, opacity: 0 }}
