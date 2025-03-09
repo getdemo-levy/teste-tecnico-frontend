bf1ce160fb99c3360b1cf7120cc4159c7d0b15a8
Levy Moreira Cruz <levymcruz@gmail.com>
2025-03-09 17:26:55 -0300

    fix: atualizando o next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://52.200.133.252:3005/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
